30f61f83409431e29530672247a5a9c4b3e174e0
Russell Jurney <russell.jurney@gmail.com>
2021-10-25 19:59:35 -0700

    On time flask demo reformatted

--- ch04/web/on_time_flask.py
+++ ch04/web/on_time_flask.py
@@ -11,35 +11,23 @@ client = MongoClient()
 # Controller: Fetch an email and display it
 @app.route("/on_time_performance")
 def on_time_performance():
-  
-  carrier = request.args.get('Carrier')
-  flight_date = request.args.get('FlightDate')
-  flight_num = request.args.get('FlightNum')
-  
-  flight = client.agile_data_science.on_time_performance.find_one({
-    'Carrier': carrier,
-    'FlightDate': flight_date,
-    'FlightNum': flight_num
-  })
-  
-  return json_util.dumps(flight)
+
+    carrier = request.args.get("Carrier")
+    flight_date = request.args.get("FlightDate")
+    flight_num = request.args.get("FlightNum")
+
+    flight = client.agile_data_science.on_time_performance.find_one(
+        {"Carrier": carrier, "FlightDate": flight_date, "FlightNum": flight_num}
+    )
+
+    return json_util.dumps(flight)
 
 
-def shutdown_server():
-  func = request.environ.get('werkzeug.server.shutdown')
-  if func is None:
-    raise RuntimeError('Not running with the Werkzeug Server')
-  func()
-
-
-@app.route('/shutdown')
-def shutdown():
-  shutdown_server()
-  return 'Server shutting down...'
+@app.route("/")
+def hello():
+    return "Hello, World!"
 
 
 if __name__ == "__main__":
-  app.run(
-    debug=True,
-    host='0.0.0.0'
-  )
+
+    app.run(debug=True, host="0.0.0.0")
