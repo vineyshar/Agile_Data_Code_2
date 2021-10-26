01c85b5f808f0f58da7690fc684b73732bb7e814
Russell Jurney <russell.jurney@gmail.com>
2021-10-26 08:34:50 -0700

    Added mongo hostname

--- ch02/web/test_flask_bootstrap.py
+++ ch02/web/test_flask_bootstrap.py
@@ -6,28 +6,28 @@ import bson.json_util
 app = Flask(__name__)
 
 # Set up Mongo
-client = MongoClient() # defaults to localhost
+client = MongoClient("mongo")  # defaults to localhost
 db = client.agile_data_science
 
 # Fetch from/to totals, given a pair of email addresses
 @app.route("/executive/<name>")
 def executive(name):
-  executives = db.executives.find({"name": name})
-  return render_template('table.html', executives=list(executives))
+    executives = db.executives.find({"name": name})
+    return render_template("table.html", executives=list(executives))
+
 
 def shutdown_server():
-  func = request.environ.get('werkzeug.server.shutdown')
-  if func is None:
-    raise RuntimeError('Not running with the Werkzeug Server')
-  func()
+    func = request.environ.get("werkzeug.server.shutdown")
+    if func is None:
+        raise RuntimeError("Not running with the Werkzeug Server")
+    func()
 
-@app.route('/shutdown')
+
+@app.route("/shutdown")
 def shutdown():
-  shutdown_server()
-  return 'Server shutting down...'
+    shutdown_server()
+    return "Server shutting down..."
 
-if __name__ == "__main__": 
-  app.run(
-    debug=True,
-    host='0.0.0.0'
-  )
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
