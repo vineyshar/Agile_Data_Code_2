19a93a1e503c8c6df2d9f95420e1787cd04db7f5
Russell Jurney <russell.jurney@gmail.com>
2021-10-27 03:29:40 -0700

    Removed shutdown method

--- ch02/web/test_flask_bootstrap.py
+++ ch02/web/test_flask_bootstrap.py
@@ -16,18 +16,5 @@ def executive(name):
     return render_template("table.html", executives=list(executives))
 
 
-def shutdown_server():
-    func = request.environ.get("werkzeug.server.shutdown")
-    if func is None:
-        raise RuntimeError("Not running with the Werkzeug Server")
-    func()
-
-
-@app.route("/shutdown")
-def shutdown():
-    shutdown_server()
-    return "Server shutting down..."
-
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
