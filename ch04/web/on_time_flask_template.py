b8579c75e5b0e0a3a4258f87ee67a6d3ff9e0f1e
Russell Jurney <russell.jurney@gmail.com>
2021-10-27 03:38:41 -0700

    Fixed navigation errors

--- ch04/web/on_time_flask_template.py
+++ ch04/web/on_time_flask_template.py
@@ -26,14 +26,14 @@ def process_search(results):
 # Calculate offsets for fetching lists of flights from MongoDB
 def get_navigation_offsets(offset1, offset2, increment):
     offsets = {}
-    offsets["Next"] = {
-        "top_offset": offset2 + increment,
-        "bottom_offset": offset1 + increment,
-    }
     offsets["Previous"] = {
         "top_offset": max(offset2 - increment, 0),
         "bottom_offset": max(offset1 - increment, 0),
     }  # Don't go < 0
+    offsets["Next"] = {
+        "top_offset": offset2 + increment,
+        "bottom_offset": offset1 + increment,
+    }
     return offsets
 
 
