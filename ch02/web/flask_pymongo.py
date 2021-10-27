af62e57cdf15b73564eff5c2a450a454af086b3f
Russell Jurney <russell.jurney@gmail.com>
2021-10-27 01:41:24 -0700

    Removed shutdown code

--- ch02/web/flask_pymongo.py
+++ ch02/web/flask_pymongo.py
@@ -6,24 +6,15 @@ import bson.json_util
 app = Flask(__name__)
 
 # Set up Mongo
-client = MongoClient() # defaults to localhost
+client = MongoClient()  # defaults to localhost
 db = client.agile_data_science
 
 # Fetch from/to totals, given a pair of email addresses
 @app.route("/executive/<name>")
 def executive(name):
-  executive = db.executives.find({"name": name})
-  return bson.json_util.dumps(list(executive))
+    executive = db.executives.find({"name": name})
+    return bson.json_util.dumps(list(executive))
 
-def shutdown_server():
-  func = request.environ.get('werkzeug.server.shutdown')
-  if func is None:
-    raise RuntimeError('Not running with the Werkzeug Server')
-  func()
 
-@app.route('/shutdown')
-def shutdown():
-  shutdown_server()
-  return 'Server shutting down...'
-
-if __name__ == "__main__": app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
